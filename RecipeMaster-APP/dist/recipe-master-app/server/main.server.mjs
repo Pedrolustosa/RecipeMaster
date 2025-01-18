25a560343a7a3712e48e3f9d194323a72cec08df
Pedrolustosa <pedroeternalss@gmail.com>
2025-01-17 22:27:44 -0300

    feat: remove dist

--- RecipeMaster-APP/dist/recipe-master-app/server/main.server.mjs
+++
@@ -1,3 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-YVKI4M4K.mjs";import"./chunk-5VIFGSNM.mjs";import"./chunk-I3M3DLOE.mjs";import"./chunk-UGFBNU6G.mjs";export{a as default};
-//# sourceMappingURL=main.server.mjs.map
