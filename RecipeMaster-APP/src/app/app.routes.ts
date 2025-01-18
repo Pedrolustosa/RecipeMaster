49920fee27b4bd24989d4cf8054c9f3a9706fda4
Pedrolustosa <pedroeternalss@gmail.com>
2025-01-18 15:05:07 -0300

    feat: route dashboard

--- RecipeMaster-APP/src/app/app.routes.ts
+++ RecipeMaster-APP/src/app/app.routes.ts
@@ -14,7 +14,7 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { 
-    path: '', 
+    path: 'dashboard', 
     component: DashboardComponent,
     canActivate: [authGuard]
   },
