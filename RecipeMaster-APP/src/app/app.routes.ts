cc8a229b8614fba60b9e385d5543b7b32cd10e13
Pedrolustosa <pedroeternalss@gmail.com>
2025-01-18 13:53:56 -0300

    feat: add vercel json

--- RecipeMaster-APP/src/app/app.routes.ts
+++ RecipeMaster-APP/src/app/app.routes.ts
@@ -36,5 +36,5 @@ export const routes: Routes = [
       { path: 'edit/:id', component: RecipeEditComponent }
     ]
   },
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: 'login' }
 ];
