02b41b869e08503496b769bc3cf3700fa9e69058
Pedrolustosa <pedroeternalss@gmail.com>
2025-01-26 00:35:48 -0300

    refactor(mapping): separate mapping profiles for Ingredients and Recipes
    - Created IngredientMappingProfile for all Ingredient-related mappings
    - Created RecipeMappingProfile for all Recipe-related mappings
    - Updated AutoMapperSetup to register individual mapping profiles
    - Improved modularity and maintainability by organizing mappings by domain

---
+++ RecipeMaster-API/RecipeMaster.Application/Mappings/IngredientMappingProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using RecipeMaster.Core.Entities;
+using RecipeMaster.Application.DTOs;
+using RecipeMaster.Core.ValueObjects;
+using RecipeMaster.Application.Commands.Ingredients;
+
+namespace RecipeMaster.Application.Mappings;
+
+public class IngredientMappingProfile : Profile
+{
+    public IngredientMappingProfile()
+    {
+        CreateMap<Ingredient, IngredientDTO>()
+            .ForMember(dest => dest.Unit, opt => opt.MapFrom(src => src.Unit.ToString()))
+            .ForMember(dest => dest.Cost, opt => opt.MapFrom(src => src.Cost.Value))
+            .ReverseMap()
+            .ForMember(dest => dest.Unit, opt => opt.MapFrom(src => Enum.Parse<MeasurementUnit>(src.Unit)))
+            .ForMember(dest => dest.Cost, opt => opt.MapFrom(src => new IngredientCost(src.Cost)));
+
+        CreateMap<UpdateIngredientCommand, Ingredient>()
+            .ReverseMap();
+
+        CreateMap<IngredientDTO, CreateIngredientCommand>()
+            .ReverseMap();
+
+        CreateMap<Ingredient, IngredientCostDTO>()
+            .ForMember(dest => dest.Cost, opt => opt.MapFrom(src => src.Cost.Value));
+
+        CreateMap<Ingredient, IngredientUsageDTO>()
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name));
+    }
+}
